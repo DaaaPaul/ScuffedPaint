d27287ac3e27c57456680fa648509a528ad119e8
DaaaPaul <paulpeng2008@gmail.com>
2026-04-12 22:21:03 -0400

    mini fb integrated

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include "MiniFB.h"
 
 int main() {
-	std::cout << "YOO\n";
+    mfb_window* window = mfb_open_ex("my display", 800, 600, MFB_WF_RESIZABLE);
+    if (window == NULL) {
+		return -1;
+	}
+
+    uint32_t* buffer = reinterpret_cast<uint32_t*>(malloc(800 * 600 * 4));
+
+    mfb_update_state state;
+    do {
+        // TODO: add some fancy rendering to the buffer of size 800 * 600
+		for(int i = 0; i < 800 * 600; i++) {
+			buffer[i] = MFB_RGB(255, 0, 0);
+		}
+
+        state = mfb_update_ex(window, buffer, 800, 600);
+
+        if (state != MFB_STATE_OK)
+            break;
+    } while(mfb_wait_sync(window));
+
+    free(buffer);
+    buffer = NULL;
+    window = NULL;
+
+	return 0;
 }
